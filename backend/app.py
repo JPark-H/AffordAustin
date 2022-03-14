57f376e7012cf744c007100e1d301cf50ace655c
Jay Park <jaylogictx@gmail.com>
2022-03-14 17:11:31 -0500

    created more routes temp makefile

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<img src="https://i.kym-cdn.com/photos/images/original/001/211/814/a1c.jpg" alt="cowboy" />'
+    # return '<img src="https://i.kym-cdn.com/photos/images/original/001/211/814/a1c.jpg" alt="cowboy" />'
+    return 'Hello World'
+
+@app.route("/test")
+def test():
+    return 'Test'
+
+@app.route("/test2")
+def test2():
+    return 'Test2'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
