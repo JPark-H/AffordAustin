3189b1dd9623830d3689b7f4356426d2505318ed
Jay Park <jayparktx@gmail.com>
2022-03-28 14:08:06 -0500

    reupdated unit tests

--- backend/tests.py
+++ backend/tests.py
@@ -7,26 +7,90 @@ class UnitTests(TestCase):
         self.ctx = app.app_context()
         self.ctx.push()
         self.client = app.test_client()
+        self.headers = {
+            "content-type": "application/vnd.api+json",
+            "accept": "application/vnd.api+json",
+        }
 
     def test_home(self):
-        test_client = app.test_client()
-        req = test_client.get("/")
+        req = self.client.get("/", headers=self.headers)
         self.assertEqual(req.status_code, 200)
 
+    # testing all housing data
     def test_housing(self):
-        test_client = app.test_client()
-        req = test_client.get("/Housing")
+        req = self.client.get("/api/housing", headers=self.headers)
         self.assertEqual(req.status_code, 200)
 
+    # testing singular housing data
+    def test_housing_single(self):
+        req = self.client.get("/api/housing/1", headers=self.headers)
+        self.assertEqual(req.status_code, 200)
+
+    # testing pagination of housing data
+    def test_housing_pages(self):
+        req = self.client.get(
+            "/api/housing?page[size]=3&page[number]=2", headers=self.headers
+        )
+        self.assertEqual(req.status_code, 200)
+
+    # testing pagination of housing data fail
+    def test_jobs_pages(self):
+        req = self.client.get(
+            "/api/jobs?page[size]=3&page[number]=123123", headers=self.headers
+        )
+        self.assertEqual(req.status_code, 200)
+        self.assertFalse(req.json["data"])
+
+    # testing all childcare data
     def test_childcare(self):
-        test_client = app.test_client()
-        req = test_client.get("/ChildCare")
+        req = self.client.get("/api/childcare", headers=self.headers)
         self.assertEqual(req.status_code, 200)
 
-    # def test_jobs(self):
-    #     test_client = app.test_client()
-    #     req = test_client.get("/Jobs")
-    #     self.assertEqual(req.status_code, 200)
+    # testing singular childcare data
+    def test_childcare_single(self):
+        req = self.client.get("/api/childcare/1", headers=self.headers)
+        self.assertEqual(req.status_code, 200)
+
+    # testing pagination of childcare data
+    def test_childcare_pages(self):
+        req = self.client.get(
+            "/api/childcare?page[size]=3&page[number]=2", headers=self.headers
+        )
+        self.assertEqual(req.status_code, 200)
+
+    # testing pagination of childcare data fail
+    def test_jobs_pages(self):
+        req = self.client.get(
+            "/api/childcare?page[size]=3&page[number]=123123", headers=self.headers
+        )
+        self.assertEqual(req.status_code, 200)
+        self.assertFalse(req.json["data"])
+
+    # testing singular job data
+    def test_jobs(self):
+        req = self.client.get("/api/jobs")
+        self.assertEqual(req.status_code, 200)
+
+    # testing all childcare data
+    def test_jobs_single(self):
+        req = self.client.get("/api/jobs/1", headers=self.headers)
+        self.assertEqual(req.status_code, 200)
+
+    # testing pagination of job data
+    def test_jobs_pages(self):
+        req = self.client.get(
+            "/api/jobs?page[size]=3&page[number]=2", headers=self.headers
+        )
+        self.assertIsNone(req.json["data"])
+        self.assertEqual(req.status_code, 200)
+
+    # testing pagination of job data fail
+    def test_jobs_pages(self):
+        req = self.client.get(
+            "/api/jobs?page[size]=3&page[number]=123123", headers=self.headers
+        )
+        self.assertEqual(req.status_code, 200)
+        self.assertFalse(req.json["data"])
 
 
 if __name__ == "__main__":
