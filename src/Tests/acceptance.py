5ce1a9737e922d540778f5559530c9730881edab
dinesh.k.balakrishnan <dinesh.k.balakrishnan@gmail.com>
2022-03-29 01:24:53 -0500

    Fixing YAML

--- src/Tests/acceptance.py
+++ src/Tests/acceptance.py
@@ -1,10 +1,18 @@
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+options = Options()
+options.page_load_strategy = 'normal'
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
 SERVICE = Service(ChromeDriverManager().install())
 
@@ -13,10 +21,10 @@ URL = 'https://development.d4sk31j15mtaj.amplifyapp.com/'
 MODELS = ['#/Housing/', '#/Childcare/', '#/Jobs/']
 
 def navigation():
-    driver = webdriver.Chrome(service=SERVICE)
+    driver = webdriver.Chrome(service=SERVICE, options=options)
     driver.get(URL)
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '[class="nav-link"]'))
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '[class="nav-link"]'))
     )
     
     links = driver.find_elements(by=By.CSS_SELECTOR, value='[class="nav-link"]')
@@ -24,7 +32,7 @@ def navigation():
 
     for link in links:
         href = link.get_attribute('href')
-        link_driver = webdriver.Chrome(service=SERVICE)
+        link_driver = webdriver.Chrome(service=SERVICE, options=options)
         link_driver.get(f'{URL}{href}')
         link_driver.close()
 
@@ -32,11 +40,11 @@ def navigation():
 
 def grids():
     for model in MODELS:
-        driver = webdriver.Chrome(service=SERVICE)
+        driver = webdriver.Chrome(service=SERVICE, options=options)
         driver.get(f'{URL}{model}')
 
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'card'))
+            EC.visibility_of_element_located((By.CLASS_NAME, 'card'))
         )
 
         links = driver.find_elements(by=By.CLASS_NAME, value='card')
@@ -47,7 +55,7 @@ def grids():
 def pages():
     for model in MODELS:
         for id in range(1, 101):
-            driver = webdriver.Chrome(service=SERVICE)
+            driver = webdriver.Chrome(service=SERVICE, options=options)
             driver.get(f'{URL}{model}{id}')
             driver.close()
 
